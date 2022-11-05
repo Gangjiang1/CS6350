4519adf452a9de33c6e577f1fe54a44e1923d5f4
Gang Jiang <114104345+Gangjiang1@users.noreply.github.com>
2022-11-04 17:40:28 -0700

    Delete run.sh

--- run.sh
+++
@@ -1,3 +0,0 @@
-python3 Driver.py ./bank-note/train.csv ./bank-note/test.csv perceptron
-python3 Driver.py ./bank-note/train.csv ./bank-note/test.csv voted_perceptron
-python3 Driver.py ./bank-note/train.csv ./bank-note/test.csv averaged_perceptron
